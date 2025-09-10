ac27c58b0267923fc056b56b5f4b190c3005b221
sanskriti singh <86918638+sanskriti-singh-code@users.noreply.github.com>
2025-09-11 00:16:26 +0530

    Configure Django to serve React app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
